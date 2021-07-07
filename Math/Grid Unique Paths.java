d538a2833a7eb1f639d285562dbc1228f7897ac9
Abhishek kumar <61092638+a4abhishekkmr@users.noreply.github.com>
2021-07-07 23:29:10 +0530

    Update Grid Unique Paths.java

--- Math/Grid Unique Paths.java
+++ Math/Grid Unique Paths.java
@@ -29,3 +29,22 @@ Output : 2
         return uniquePaths(A-1,B)+uniquePaths(A,B-1);
     }
 }
+//dp solution
+
+        int dp[][]=new int[A+1][B+1];
+        for(int i=0;i<A+1;i++)
+        dp[i][1]=1;
+        for(int i=0;i<B+1;i++)
+        dp[1][i]=1;
+
+        for(int i=2;i<A+1;i++)
+        {
+            for(int j=2;j<B+1;j++)
+            {
+                dp[i][j]=dp[i-1][j]+dp[i][j-1];
+            }
+        }
+        // return uniquePaths(A-1,B)+uniquePaths(A,B-1);
+        return dp[A][B];
+    }
+}
